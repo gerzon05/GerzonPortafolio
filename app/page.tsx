458a6f6a18108eea9485c1551c81923867ff0876
Gerzon05 <gerzonarletrangel@gmail.com>
2025-01-25 16:09:02 -0500

    feat: loading page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
-import Header from '@/src/components/header';
-import { Container } from '../styled-system/jsx';
+import Header from '@/src/components/header'
+import ButtonReload from '@/src/components/ui/button-reload'
+import Contact from '@/src/page/contact'
+import Footer from '@/src/page/footer'
+import Presentation from '@/src/page/presentation'
+import Proyect from '@/src/page/proyect'
+import WorkExperience from '@/src/page/work-experience'
+import { Container } from '../styled-system/jsx'
 
 export default function Home() {
   return (
     <Container>
+      <ButtonReload />
       <Header />
+      <Presentation />
+      <WorkExperience />
+      <Proyect />
+      <Contact />
+      <Footer />
     </Container>
   )
 }
