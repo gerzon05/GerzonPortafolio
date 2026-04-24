c724b88914628944d8ed5a827b5c2bda9c16d48f
gerzon05 <gerzonarletrangel@gmail.com>
2026-04-23 22:52:08 -0500

    chore: setup proyecto with `nuxt4`

---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  alias: {
+    'nitropack/runtime/internal/config': '@nuxt/nitro-server/dist/runtime/utils/app-config'
+  }
+})
